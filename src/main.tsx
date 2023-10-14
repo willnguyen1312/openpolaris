1ce3cd44c1ba2a487510a47068cfb4fb5d6db09a
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-10-14 17:56:02 -0300

    Bring Polaris to life 💞

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import "@shopify/polaris/build/esm/styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 1312,
+    open: true,
+  },
 });
