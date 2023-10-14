b3156bc279944c94928dd103455a66b19d75431a
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-10-14 18:22:24 -0300

    Add TopBar example 🚀

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,118 @@
-import { AppProvider, Box, Button } from "@shopify/polaris";
-import enTranslations from "@shopify/polaris/locales/en.json";
+import { ActionList, Frame, Icon, Text, TopBar } from "@shopify/polaris";
+import { ArrowLeftMinor, QuestionMarkMajor } from "@shopify/polaris-icons";
+import { useCallback, useState } from "react";
 
-export default function App() {
+export default function TopBarExample() {
+  const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
+  const [isSecondaryMenuOpen, setIsSecondaryMenuOpen] = useState(false);
+  const [isSearchActive, setIsSearchActive] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+
+  const toggleIsUserMenuOpen = useCallback(
+    () => setIsUserMenuOpen((isUserMenuOpen) => !isUserMenuOpen),
+    [],
+  );
+
+  const toggleIsSecondaryMenuOpen = useCallback(
+    () => setIsSecondaryMenuOpen((isSecondaryMenuOpen) => !isSecondaryMenuOpen),
+    [],
+  );
+
+  const handleSearchResultsDismiss = useCallback(() => {
+    setIsSearchActive(false);
+    setSearchValue("");
+  }, []);
+
+  const handleSearchChange = useCallback((value: string) => {
+    setSearchValue(value);
+    setIsSearchActive(value.length > 0);
+  }, []);
+
+  const handleNavigationToggle = useCallback(() => {
+    console.log("toggle navigation visibility");
+  }, []);
+
+  const logo = {
+    width: 124,
+    topBarSource:
+      "https://cdn.shopify.com/s/files/1/0446/6937/files/jaded-pixel-logo-color.svg?6215648040070010999",
+    url: "#",
+    accessibilityLabel: "Jaded Pixel",
+  };
+
+  const userMenuMarkup = (
+    <TopBar.UserMenu
+      actions={[
+        {
+          items: [{ content: "Back to Shopify", icon: ArrowLeftMinor }],
+        },
+        {
+          items: [{ content: "Community forums" }],
+        },
+      ]}
+      name="Dharma"
+      detail="Jaded Pixel"
+      initials="D"
+      open={isUserMenuOpen}
+      onToggle={toggleIsUserMenuOpen}
+    />
+  );
+
+  const searchResultsMarkup = (
+    <ActionList
+      items={[
+        { content: "Shopify help center" },
+        { content: "Community forums" },
+      ]}
+    />
+  );
+
+  const searchFieldMarkup = (
+    <TopBar.SearchField
+      onChange={handleSearchChange}
+      value={searchValue}
+      placeholder="Search"
+      showFocusBorder
+    />
+  );
+
+  const secondaryMenuMarkup = (
+    <TopBar.Menu
+      activatorContent={
+        <span>
+          <Icon source={QuestionMarkMajor} />
+          <Text as="span" visuallyHidden>
+            Secondary menu
+          </Text>
+        </span>
+      }
+      open={isSecondaryMenuOpen}
+      onOpen={toggleIsSecondaryMenuOpen}
+      onClose={toggleIsSecondaryMenuOpen}
+      actions={[
+        {
+          items: [{ content: "Community forums" }],
+        },
+      ]}
+    />
+  );
+
+  const topBarMarkup = (
+    <TopBar
+      showNavigationToggle
+      userMenu={userMenuMarkup}
+      secondaryMenu={secondaryMenuMarkup}
+      searchResultsVisible={isSearchActive}
+      searchField={searchFieldMarkup}
+      searchResults={searchResultsMarkup}
+      onSearchResultsDismiss={handleSearchResultsDismiss}
+      onNavigationToggle={handleNavigationToggle}
+    />
+  );
+
   return (
-    <AppProvider i18n={enTranslations}>
-      <Box
-        padding={{
-          xs: "200",
-        }}
-      >
-        <Button
-          variant="primary"
-          tone="success"
-          onClick={() => alert("Button clicked!")}
-        >
-          Example button
-        </Button>
-      </Box>
-    </AppProvider>
+    <div style={{ height: "250px" }}>
+      <Frame topBar={topBarMarkup} logo={logo} />
+    </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+import { AppProvider } from "@shopify/polaris";
 import "@shopify/polaris/build/esm/styles.css";
+import enTranslations from "@shopify/polaris/locales/en.json";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <AppProvider i18n={enTranslations}>
+      <App />
+    </AppProvider>
   </React.StrictMode>,
 );
