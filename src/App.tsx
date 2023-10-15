c81e9de7e43d6fe7777839db69475d076678ab4f
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-10-15 09:16:54 -0300

    Add app setting layout pattern 💞

--- src/App.tsx
+++ src/App.tsx
@@ -1,118 +1,77 @@
-import { ActionList, Frame, Icon, Text, TopBar } from "@shopify/polaris";
-import { ArrowLeftMinor, QuestionMarkMajor } from "@shopify/polaris-icons";
-import { useCallback, useState } from "react";
+import {
+  BlockStack,
+  Box,
+  Card,
+  Divider,
+  InlineGrid,
+  Page,
+  Text,
+  TextField,
+  useBreakpoints,
+} from "@shopify/polaris";
 
-export default function TopBarExample() {
-  const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
-  const [isSecondaryMenuOpen, setIsSecondaryMenuOpen] = useState(false);
-  const [isSearchActive, setIsSearchActive] = useState(false);
-  const [searchValue, setSearchValue] = useState("");
-
-  const toggleIsUserMenuOpen = useCallback(
-    () => setIsUserMenuOpen((isUserMenuOpen) => !isUserMenuOpen),
-    [],
-  );
-
-  const toggleIsSecondaryMenuOpen = useCallback(
-    () => setIsSecondaryMenuOpen((isSecondaryMenuOpen) => !isSecondaryMenuOpen),
-    [],
-  );
-
-  const handleSearchResultsDismiss = useCallback(() => {
-    setIsSearchActive(false);
-    setSearchValue("");
-  }, []);
-
-  const handleSearchChange = useCallback((value: string) => {
-    setSearchValue(value);
-    setIsSearchActive(value.length > 0);
-  }, []);
-
-  const handleNavigationToggle = useCallback(() => {
-    console.log("toggle navigation visibility");
-  }, []);
-
-  const logo = {
-    width: 124,
-    topBarSource:
-      "https://cdn.shopify.com/s/files/1/0446/6937/files/jaded-pixel-logo-color.svg?6215648040070010999",
-    url: "#",
-    accessibilityLabel: "Jaded Pixel",
-  };
-
-  const userMenuMarkup = (
-    <TopBar.UserMenu
-      actions={[
-        {
-          items: [{ content: "Back to Shopify", icon: ArrowLeftMinor }],
-        },
-        {
-          items: [{ content: "Community forums" }],
-        },
-      ]}
-      name="Dharma"
-      detail="Jaded Pixel"
-      initials="D"
-      open={isUserMenuOpen}
-      onToggle={toggleIsUserMenuOpen}
-    />
-  );
-
-  const searchResultsMarkup = (
-    <ActionList
-      items={[
-        { content: "Shopify help center" },
-        { content: "Community forums" },
-      ]}
-    />
-  );
-
-  const searchFieldMarkup = (
-    <TopBar.SearchField
-      onChange={handleSearchChange}
-      value={searchValue}
-      placeholder="Search"
-      showFocusBorder
-    />
-  );
-
-  const secondaryMenuMarkup = (
-    <TopBar.Menu
-      activatorContent={
-        <span>
-          <Icon source={QuestionMarkMajor} />
-          <Text as="span" visuallyHidden>
-            Secondary menu
-          </Text>
-        </span>
-      }
-      open={isSecondaryMenuOpen}
-      onOpen={toggleIsSecondaryMenuOpen}
-      onClose={toggleIsSecondaryMenuOpen}
-      actions={[
-        {
-          items: [{ content: "Community forums" }],
-        },
-      ]}
-    />
-  );
-
-  const topBarMarkup = (
-    <TopBar
-      showNavigationToggle
-      userMenu={userMenuMarkup}
-      secondaryMenu={secondaryMenuMarkup}
-      searchResultsVisible={isSearchActive}
-      searchField={searchFieldMarkup}
-      searchResults={searchResultsMarkup}
-      onSearchResultsDismiss={handleSearchResultsDismiss}
-      onNavigationToggle={handleNavigationToggle}
-    />
-  );
-
+// This example is for guidance purposes. Copying it will come with caveats.
+export default function AppSettingsLayoutExample() {
+  const { smUp } = useBreakpoints();
   return (
-    <div style={{ height: "250px" }}>
-      <Frame topBar={topBarMarkup} logo={logo} />
-    </div>
+    <Page
+      // divider
+      primaryAction={{ content: "View on your store", disabled: true }}
+      secondaryActions={[
+        {
+          content: "Duplicate",
+          accessibilityLabel: "Secondary action label",
+          onAction: () => alert("Duplicate action"),
+        },
+      ]}
+    >
+      <BlockStack gap={{ xs: "800", sm: "400" }}>
+        <InlineGrid columns={{ xs: "1fr", md: "2fr 5fr" }} gap="400">
+          <Box
+            as="section"
+            paddingInlineStart={{ xs: "400", sm: "0" }}
+            paddingInlineEnd={{ xs: "400", sm: "0" }}
+          >
+            <BlockStack gap="400">
+              <Text as="h3" variant="headingMd">
+                InterJambs
+              </Text>
+              <Text as="p" variant="bodyMd">
+                Interjambs are the rounded protruding bits of your puzzlie piece
+              </Text>
+            </BlockStack>
+          </Box>
+          <Card roundedAbove="sm">
+            <BlockStack gap="400">
+              <TextField label="Interjamb style" autoComplete="off" />
+              <TextField label="Interjamb ratio" autoComplete="off" />
+            </BlockStack>
+          </Card>
+        </InlineGrid>
+        {smUp ? <Divider /> : null}
+        <InlineGrid columns={{ xs: "1fr", md: "2fr 5fr" }} gap="400">
+          <Box
+            as="section"
+            paddingInlineStart={{ xs: "400", sm: "0" }}
+            paddingInlineEnd={{ xs: "400", sm: "0" }}
+          >
+            <BlockStack gap="400">
+              <Text as="h3" variant="headingMd">
+                Dimensions
+              </Text>
+              <Text as="p" variant="bodyMd">
+                Interjambs are the rounded protruding bits of your puzzlie piece
+              </Text>
+            </BlockStack>
+          </Box>
+          <Card roundedAbove="sm">
+            <BlockStack gap="400">
+              <TextField label="Horizontal" autoComplete="off" />
+              <TextField label="Interjamb ratio" autoComplete="off" />
+            </BlockStack>
+          </Card>
+        </InlineGrid>
+      </BlockStack>
+    </Page>
   );
 }
