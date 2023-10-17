2cf93a935c4a5ca5d60186c151e934c7d2abd277
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-10-17 15:59:56 -0300

    Add splitVendorChunkPlugin to build 💞

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
 });
