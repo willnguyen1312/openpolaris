be5f3e96ca45a36058586cbae4587460d8f705bc
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-10-20 10:35:07 -0300

    Change root component id to main 🚀

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App.tsx";
 
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("app")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
