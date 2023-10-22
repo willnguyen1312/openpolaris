b962a0404c0c3000307db67e3afe5ba0a5fae743
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-10-22 14:55:25 -0300

    Add dnd support for complex components 💞

--- src/store/index.ts
+++ src/store/index.ts
@@ -294,6 +294,23 @@ const useStoreBase = createWithEqualityFn(
                 state.renderedComponents = state.renderedComponents.filter(
                   (component) => component.id !== activeId,
                 );
+                return;
+              }
+
+              const canHaveAnyChildren =
+                acceptComponentsMap[overContainer.componentName]?.type ===
+                ComponentAcceptType.ParentWithAnyChildren;
+
+              if (canHaveAnyChildren) {
+                let index = overContainer.children.findIndex(
+                  (component) => component.id === overId,
+                );
+                index = index === -1 ? overContainer.children.length : index;
+                overContainer.children.splice(index, 0, activeComponent);
+                state.renderedComponents = state.renderedComponents.filter(
+                  (component) => component.id !== activeId,
+                );
+                return;
               }
             }
 
