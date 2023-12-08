8a4dd1feca386ab106fbea3654cda4af21b445cc
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-12-08 13:47:40 -0400

    Add auto-optimization from million 🎉

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react-swc";
+import million from "million/compiler";
 import { defineConfig, splitVendorChunkPlugin } from "vite";
 
 export default defineConfig({
-  plugins: [react(), splitVendorChunkPlugin()],
+  plugins: [million.vite({ auto: true }), react(), splitVendorChunkPlugin()],
 });
