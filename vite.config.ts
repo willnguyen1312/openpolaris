fa95f35267b8e1d47ef6a2566947a8bc19ef4af6
Nam Nguyen <nam.nguyen@shopify.com>
2024-02-11 22:27:36 -0400

    chore: remove unnecessary million compiler 💞

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import react from "@vitejs/plugin-react-swc";
-import million from "million/compiler";
+
 import { defineConfig, splitVendorChunkPlugin } from "vite";
 
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), react(), splitVendorChunkPlugin()],
+  plugins: [react(), splitVendorChunkPlugin()],
 });
